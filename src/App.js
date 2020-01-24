f8c34cdd6ef0ddcb504711d97c68c6d9f24db211
Marcus Notheis <marcus.notheis@sap.com>
2020-01-24 12:27:49 +0100

    Add absolute scroll container around app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { ThemeProvider } from '@ui5/webcomponents-react/lib/ThemeProvider';
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom'
-import StoreDetail from "./detail";
-import Home from "./home";
-import AppBar from "./appbar";
+import { HashRouter, Route } from 'react-router-dom';
+import StoreDetail from './detail';
+import Home from './home';
+import AppBar from './appbar';
 
 function App() {
   return (
     <ThemeProvider withToastContainer>
-
       <AppBar />
 
-      <HashRouter>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/stores/:storeId" component={StoreDetail} />
-      </HashRouter>
+      <div style={{ position: 'absolute', top: '2.75rem', bottom: 0, left: 0, right: 0, overflowY: 'auto' }}>
+        <HashRouter>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/stores/:storeId" component={StoreDetail} />
+        </HashRouter>
+      </div>
     </ThemeProvider>
   );
 }
